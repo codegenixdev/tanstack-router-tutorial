6ccf302039288c9b24b67da27352ce9d8991832e
codegenixdev <czhablog@gmail.com>
2025-08-24 13:28:17 +0330

    splat

--- src/routes/account/files/$/index.tsx
+++ src/routes/account/files/$/index.tsx
@@ -1,4 +1,5 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { useState } from "react";
 
 export const Route = createFileRoute("/account/files/$/")({
   component: RouteComponent,
@@ -6,9 +7,20 @@ export const Route = createFileRoute("/account/files/$/")({
 
 function RouteComponent() {
   const { _splat } = Route.useParams();
+  const [filepath, setFilepath] = useState(_splat);
+
   return (
     <div>
-      <h1>File: {_splat}</h1>
+      <input
+        className="border border-gray-300 rounded-md p-2"
+        type="text"
+        value={filepath}
+        onChange={(e) => setFilepath(e.target.value)}
+      />
+      <Link to="/account/files/$" params={{ _splat: filepath }}>
+        Go to file
+      </Link>
+      {_splat ? <h1>File: {_splat}</h1> : <h1>No file found</h1>}
     </div>
   );
 }
--- src/routes/account/route.tsx
+++ src/routes/account/route.tsx
@@ -1,11 +1,9 @@
 import {
   createFileRoute,
-  getRouteApi,
   Link,
   Outlet,
   redirect,
 } from "@tanstack/react-router";
-import { useState } from "react";
 
 const isAuthenticated = true;
 export const Route = createFileRoute("/account")({
@@ -22,21 +20,10 @@ export const Route = createFileRoute("/account")({
   },
 });
 
-const filesRouterApi = getRouteApi("/account/files/$/");
 function RouteComponent() {
-  const { _splat } = filesRouterApi.useParams();
-  const [filepath, setFilepath] = useState(_splat);
   return (
     <div>
-      <input
-        className="border border-gray-300 rounded-md p-2"
-        type="text"
-        value={filepath}
-        onChange={(e) => setFilepath(e.target.value)}
-      />
-      <Link to="/account/files/$" params={{ _splat: filepath }}>
-        Go to file
-      </Link>
+      <Link to="/account/files/$">Files</Link>
       <Outlet />
     </div>
   );
